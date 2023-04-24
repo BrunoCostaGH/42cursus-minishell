60cba5eac578f8880b3c68bb5a77dfa1ec70c48c
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-04-24 19:04:33 +0100

    fix incorrect environment lookup

--- exec.c
+++ exec.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 21:13:32 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/04/24 15:20:53 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/04/24 18:55:57 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,35 @@
 /* Check if argv[0] is an environment variable and if true set argv[0] as the
  * address in the environment.
  */
-static void	check_environment(void *path)
+static char	*check_environment(char *fname)
 {
-	char	*env;
-	char	*final_path;
+	int		i;
+	char	*name;
+	char	*path;
+	char	**little_path;
 
-	if (!path)
-		return ;
-	env = ft_calloc(ft_strlen(path) + 1, sizeof(char));
-	if (!env)
-		return ;
-	env = (char *)ft_memcpy(env, path, ft_strlen(path));
-	env = (char *)str_toupper(env);
-	final_path = getenv(env);
-	if (final_path)
+	i = 0;
+	name = 0;
+	path = getenv("PATH");
+	little_path = ft_split(path, ':');
+	while (little_path[i])
 	{
+		path = ft_strjoin(little_path[i], "/");
+		name = ft_strjoin(path, fname);
 		free(path);
-		path = final_path;
+		if (access(name, F_OK) == 0)
+			break ;
+		free(name);
+		name = 0;
+		i++;
 	}
-	free(env);
+	if (name)
+	{
+		free(fname);
+		fname = name;
+	}
+	free_darr((void **)little_path);
+	return (name);
 }
 
 void	run_executable(char *prompt)
