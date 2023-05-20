53e7bbd120e88b5574628b8549274c099851e5ef
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-05-20 16:10:13 +0100

    ENVP Reworked
    add ENVP to own struct
    add exec_envp to be build prior to execve call

---
+++ export.c
@@ -0,0 +1,128 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/19 19:42:05 by bsilva-c          #+#    #+#             */
+/*   Updated: 2023/05/20 15:21:12 by bsilva-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+static int	check_argv(t_data *data, char *arg, int initial)
+{
+	int		index;
+	char	*temp;
+
+	index = 0;
+	if (initial == TRUE && !arg)
+	{
+		while (data->envp.envp[index])
+		{
+			printf("%s %s", "declare -x", data->envp.envp[index][0]);
+			if (data->envp.envp[index][1])
+				printf("=\"%s\"", data->envp.envp[index][1]);
+			printf("\n");
+			index++;
+		}
+		return (1);
+	}
+	while (initial == FALSE && arg[index])
+	{
+		if (!ft_isalpha(arg[index]) && arg[index] != '_')
+		{
+			temp = ft_strjoin("export: ", arg);
+			handle_error(data, temp, 2);
+			free(temp);
+			return (1);
+		}
+		index++;
+	}
+	return (0);
+}
+
+static char	***duplicate_envp(t_data *data, int len)
+{
+	int		i;
+	char	***temp_envp;
+
+	i = 0;
+	temp_envp = ft_calloc(len + len_darr((void **)data->envp.envp) + 1, \
+	sizeof(char **));
+	while (data->envp.envp[i])
+	{
+		temp_envp[i] = ft_calloc(2, sizeof(char *));
+		temp_envp[i][0] = data->envp.envp[i][0];
+		temp_envp[i][1] = data->envp.envp[i][1];
+		i++;
+	}
+	return (temp_envp);
+}
+
+static int	edit_existing_envp(t_data *data, char **argv)
+{
+	int		i;
+	int		index_argv;
+	int		amount_edited;
+	char	**temp;
+
+	amount_edited = 0;
+	index_argv = 0;
+	while (argv[++index_argv])
+	{
+		i = 0;
+		temp = ft_split(argv[index_argv], '=');
+		while (data->envp.envp[i] && ft_strncmp(data->envp.envp[i][0], temp[0], \
+		ft_strlen(data->envp.envp[i][0])))
+			i++;
+		if (data->envp.envp[i])
+		{
+			free(data->envp.envp[i][1]);
+			data->envp.envp[i][1] = ft_strdup(temp[1]);
+			free(argv[index_argv]);
+			i = index_argv;
+			while (argv[++i])
+				argv[i - 1] = argv[i];
+			argv[--i] = 0;
+			amount_edited++;
+		}
+		free_darr((void **) temp);
+	}
+	return (amount_edited);
+}
+
+void	export(t_data *data, char **argv)
+{
+	int		index_argv;
+	int		index_env;
+	char	**temp;
+	char	***temp_envp;
+
+	if (check_argv(data, argv[1], TRUE))
+		return ;
+	index_argv = len_darr((void **)argv) - edit_existing_envp(data, argv);
+	if (!index_argv)
+		return ;
+	temp_envp = duplicate_envp(data, index_argv);
+	index_env = len_darr((void **)temp_envp);
+	index_argv = 0;
+	while (argv[++index_argv])
+	{
+		temp = ft_split(argv[index_argv], '=');
+		if (check_argv(data, temp[0], FALSE))
+		{
+			free_darr((void **)temp);
+			continue ;
+		}
+		temp_envp[index_env] = ft_calloc(2, sizeof(char *));
+		temp_envp[index_env][0] = ft_strdup(temp[0]);
+		temp_envp[index_env][1] = ft_strdup(temp[1]);
+		free_darr((void **)temp);
+		index_env++;
+	}
+	free(data->envp.envp);
+	data->envp.envp = temp_envp;
+}
--- init.c
+++ init.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 14:16:15 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/05/18 16:17:31 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/05/19 18:18:41 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,16 @@ static void	init_envp(t_data *data, char **envp)
 	len = 0;
 	while (envp[len])
 		len++;
-	data->envp = ft_calloc(len + 1, sizeof(char **));
-	if (!data->envp)
+	data->envp.exec_envp = 0;
+	data->envp.envp = malloc(sizeof(char **) * len + 1);
+	if (!data->envp.envp)
 		return ;
 	while (len-- > 0)
 	{
-		data->envp[len] = malloc(sizeof(char *) * 2);
 		temp = ft_split(envp[len], '=');
-		data->envp[len][0] = temp[0];
-		data->envp[len][1] = temp[1];
+		data->envp.envp[len] = ft_calloc(2, sizeof(char *));
+		data->envp.envp[len][0] = temp[0];
+		data->envp.envp[len][1] = temp[1];
 		free(temp);
 	}
 }
