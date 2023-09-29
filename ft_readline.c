34b695f917bed47a3748afb60e2d965279d5c12c
bsilva-c <54143084+BrunoCostaGH@users.noreply.github.com>
2023-09-29 19:59:04 +0100

    fix writing within string add insert key

--- ft_readline.c
+++ ft_readline.c
@@ -6,7 +6,7 @@
 /*   By: bsilva-c <bsilva-c@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 17:56:27 by bsilva-c          #+#    #+#             */
-/*   Updated: 2023/09/28 19:56:38 by bsilva-c         ###   ########.fr       */
+/*   Updated: 2023/09/29 19:55:59 by bsilva-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,58 @@
  * RETURN VALUE
  * Returns a memory allocated string with the updated string
  */
+static char	*ft_strerase(const char *string, size_t pos)
+{
+	char	*temp;
+	char	*_return;
+
+	if (!string && !pos)
+		return (ft_strdup(string));
+	else if (!string)
+		return (0);
+	temp = ft_calloc(pos + 1, sizeof(char));
+	(void)ft_strlcpy(temp, string, pos);
+	if ((size_t)ft_strlen(string) >= pos)
+	{
+		_return = ft_strjoin(temp, string + pos);
+		free(temp);
+	}
+	else
+		_return = temp;
+	return (_return);
+}
+
+/*
+ * RETURN VALUE
+ * Returns a memory allocated string with the updated string
+ */
 static char	*ft_strinsert(const char *string, char *to_insert, size_t pos)
 {
 	char	*temp;
 	char	*_return;
 
+	if (!string && !pos)
+		return (ft_strdup(to_insert));
+	else if (!string)
+		return (0);
 	_return = ft_calloc(pos + 1, sizeof(char));
 	(void)ft_strlcpy(_return, string, pos + 1);
 	temp = ft_strjoin(_return, to_insert);
 	if (_return)
 		free(_return);
-	_return = ft_strjoin(temp, string + pos);
-	if (temp)
+	if ((size_t)ft_strlen(string) >= pos)
+	{
+		_return = ft_strjoin(temp, string + pos);
 		free(temp);
+	}
+	else
+		_return = temp;
 	return (_return);
 }
 
 /*
- * TODO fix printing after writing within string
+ * TODO add redraw of terminal window
  * TODO fix cursor after using tab [tab is disabled for now]
- * TODO add history
  * TODO add tab autocomplete
  */
 static void	print_char(t_readline *rl_data, char *string)
@@ -51,7 +83,7 @@ static void	print_char(t_readline *rl_data, char *string)
 			;//ft_printf("\x1b[1A");
 		else if (ft_tolower(buf[1]) == 'b')
 			;//ft_printf("\x1b[1B");
-		else if (ft_tolower(buf[1]) == 'c' && \
+		else if (ft_tolower(buf[1]) == 'c' && rl_data->input && \
 			rl_data->cursor_pos + 1 <= ft_strlen(rl_data->input))
 		{
 			ft_printf("\x1b[1C");
@@ -63,14 +95,13 @@ static void	print_char(t_readline *rl_data, char *string)
 			rl_data->cursor_pos--;
 		}
 		else if (ft_tolower(buf[1]) == '2')
-			;//TODO INSERT KEY
+			read(0, &buf[0], 1);
 		else if (ft_tolower(buf[1]) == '3')
 			;//TODO DELETE KEY
 	}
-	else if (*string == 0x04)
-		*string = 0;
 	else if (*string == 0x7f && ft_strlen(rl_data->input))
 	{
+		//TODO implement ft_strerase();
 		ft_printf("\b \b");
 		rl_data->input[ft_strlen(rl_data->input) - 1] = 0;
 	}
@@ -91,30 +122,37 @@ static void	get_user_input(t_readline *rl_data, const char *prompt)
 	while (!_exit)
 	{
 		if (rl_data->input)
+		{
 			free(rl_data->input);
-		rl_data->input = ft_calloc(1, sizeof(char));
-		if (!rl_data->input)
-			return ;
+			rl_data->input = 0;
+		}
 		ft_printf("%s", prompt);
 		read_bytes = read(0, &buf, 1);
 		buf[read_bytes] = '\0';
 		while (read_bytes > 0)
 		{
-			if (ft_isprint(buf[0]) || (buf[0] >= 7 && buf[0] <= 13 && buf[0] != 9))
+			if (ft_isprint(buf[0]) || \
+				(buf[0] >= 7 && buf[0] <= 13 && buf[0] != 9 && buf[0] != 10))
 			{
 				temp = rl_data->input;
-				rl_data->input = ft_strinsert(rl_data->input, buf, rl_data->cursor_pos);
+				rl_data->input = ft_strinsert(rl_data->input, buf, \
+					rl_data->cursor_pos);
+				ft_printf("%s", rl_data->input + rl_data->cursor_pos);
 				for (int i = rl_data->cursor_pos; i < ft_strlen(rl_data->input) - 1; i++)
 				{
 					ft_printf("\x1b[1D");
 				}
-				ft_printf("%s", rl_data->input + rl_data->cursor_pos);
 				rl_data->cursor_pos++;
-				free(temp);
+				if (temp)
+					free(temp);
 			}
 			print_char(rl_data, &buf[0]);
-			if (ft_strchr(buf, '\n') || \
-				(buf[0] == 0 && !ft_strlen(rl_data->input)))
+			if ((buf[0] == '\n' && !rl_data->input))
+			{
+				ft_printf("\n");
+				break ;
+			}
+			if (buf[0] == '\n' || (buf[0] == 0x04 && !rl_data->input))
 			{
 				_exit = true;
 				break ;
@@ -129,11 +167,10 @@ static void	get_user_input(t_readline *rl_data, const char *prompt)
  * RETURN VALUE
  * Returns the current input, without waiting for user prompt
  */
-static char	*get_input(const char *prompt)
+static void	get_input(t_readline *rl_data, const char *prompt)
 {
 	struct termios	old;
 	struct termios	new;
-	t_readline		rl_data;
 
 	tcgetattr(STDIN_FILENO, &old);
 	new = old;
@@ -144,20 +181,17 @@ static char	*get_input(const char *prompt)
 	new.c_cc[VWERASE] = 0;
 	new.c_cc[VQUIT] = 0;
 	tcsetattr(STDIN_FILENO, TCSANOW, &new);
-	rl_data.input = 0;
-	rl_data.cursor_pos = 0;
-	get_user_input(&rl_data, prompt);
+	get_user_input(rl_data, prompt);
 	tcsetattr(STDIN_FILENO, TCSANOW, &old);
-	return (rl_data.input);
 }
 
 char	*ft_readline(const char *prompt)
 {
-	char	*temp;
-	char	*_return;
+	t_readline		rl_data;
 
-	temp = get_input(prompt);
-	_return = ft_fndnrepl(temp, "\n", "\0");
-	free(temp);
-	return (_return);
+	rl_data.input = 0;
+	rl_data.cursor_pos = 0;
+	get_input(&rl_data, prompt);
+	ft_printf("\n");
+	return (rl_data.input);
 }
